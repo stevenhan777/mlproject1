78ed19b00ca70cf82ae8779904d5bc88e7ca32e1
stevenhan777 <120138752+stevenhan777@users.noreply.github.com>
2025-05-20 08:08:35 -0400

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -58,10 +58,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
-# def load_object(file_path):
-#     try:
-#         with open(file_path, "rb") as file_obj:
-#             return pickle.load(file_obj)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)  # change from pickle
 
-#     except Exception as e:
-#         raise CustomException(e, sys)
+    except Exception as e:
+        raise CustomException(e, sys)
